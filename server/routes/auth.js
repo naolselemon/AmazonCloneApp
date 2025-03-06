aed8a4137919fb64103ec01d8ace3fc85cb0bb21
naolselemon <naol.selemon.scitech@gmail.com>
2025-03-06 17:13:49 +0300

    server reinitiated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import bcrypt from 'bcryptjs';
 
 import User from '../models/user.js';
 
@@ -15,14 +16,14 @@ authRouter.post("/auth/register", async(req, res)=> {
     }
 
  // create new user
-
-    const user = new User({name, email, password});
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({name, email, password: hashedPassword});
     await user.save();
 
     res.status(201).json({message: "User registered successfully", user});
 
     }catch(e){
-        res.status(500).json({error: "Failed to register user"});
+        res.status(500).json({error: "Failed to register user", e});
     }
 })
 
